233ab497b8518f38aeaf9a53e4fb721ee9cdddc6
navyareddy15 <122139863+navyareddy15@users.noreply.github.com>
2023-01-08 20:28:09 +0530

    Update spotify.py

--- spotify.py
+++ spotify.py
@@ -129,7 +129,14 @@ st.sidebar.image(image3)
 # Background image
 import base64
 
-main_bg = "images/https://www.whiteboardjournal.com/wp-content/uploads/2022/05/Spotify-Highlark-Feature-1280x860.gif"
+page_bg_img = """
+<style>
+[data-testid= "stAppViewContainer"] {
+background-image: url("https://www.whiteboardjournal.com/wp-content/uploads/2022/05/Spotify-Highlark-Feature-1280x860.gif")
+background-size: cover;
+}
+
+main_bg = "images/bg.png"
 main_bg_ext = "png"
 
 side_bg = "images/bg.png"
