f155cd13a7b9ae55e85f80f485fd15a6425c6f41
navyareddy15 <122139863+navyareddy15@users.noreply.github.com>
2023-01-09 14:53:57 +0530

    Update spotify.py

--- spotify.py
+++ spotify.py
@@ -139,7 +139,7 @@ st.markdown(
     f"""
     <style>
     .reportview-container {{
-        background: url(data:image/gif/video{main_bg_ext};base64,{base64.b64encode(open(main_bg, "rb").read()).decode()})
+        background: url(data:image/gif/video{main_bg_ext};base64,{base64.b64encode(open(main_bg, "rb").read()).decode()}color='dark green')
     }}
    .sidebar .sidebar-content {{
         background: url(data:image/gif/video{side_bg_ext};base64,{base64.b64encode(open(side_bg, "rb").read()).decode()})
